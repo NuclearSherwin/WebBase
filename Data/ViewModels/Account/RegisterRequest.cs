4a63ccc71b065db1a914cd3ffa03576b57236a57
Phong Mai <phongspacenasa@gmail.com>
2023-06-04 21:48:43 +0700

    create a user

--- Data/ViewModels/Account/RegisterRequest.cs
+++ Data/ViewModels/Account/RegisterRequest.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 using Common.Constants;
+using Data.Entities.User;
 
 namespace Data.ViewModels.Account
 {
@@ -18,51 +20,23 @@ namespace Data.ViewModels.Account
         [Required]
         [Compare("Password")]
         public string ConfirmPassword { get; set; }
-        
-        [Required]
-        [MinLength(6)]
-        [MaxLength(100)]
-        public string FullName { get; set; }
-        public StringEnums.Gender Gender { get; set; }
-        [Required]
-        [MinLength(3)]
-        public string Field { get; set; }
-        [Required]
-        [MinLength(3)]
-        public string SchoolYear { get; set; }
-        [Required]
-        [MinLength(3)]
-        public string ClassName { get; set; }
-        [Required]
-        public string IdStudent { get; set; }
-        
-        public string? CitizenIdentification { get; set; }
-        [Required]
-        public string StudentCard { get; set; }
-        [JsonIgnore]
-        public string ImgPath { get; set; }
-        [Required]
-        [MinLength(3)]
-        public string PhoneNumber { get; set; }
-        public string FaceBook { get; set; }
-        public string Instagram { get; set; }
-        public string LinkedIn { get; set; }
-        public string SelfDescription { get; set; }
-        public string FavoriteMaxim { get; set; }
-        public string Skill { get; set; }
-        public string Experience { get; set; }
-        public string Advantage { get; set; }
-        public string Disadvantage { get; set; }
-        public string CareerOrientation { get; set; }
-        public string DesireCareer { get; set; }
-        
-        public Dictionary<string, object>? SupervisorInfo { get; set; }
 
 
         [Range(typeof(bool), "true", "true")]
         public bool AcceptTerms { get; set; }
         
-        [Required] 
-        public StringEnums.Roles Role { get; set; }
+        // [Required] 
+        // public StringEnums.Roles Role { get; set; }
+        
+        public string FirstName { get; set; } // First name of the user
+        
+        public string LastName { get; set; } // Last name of the 
+
+        public string PhoneNumber { get; set; } // Phone number of the user
+        
+        public DateTime DateOfBirth { get; set; } // Date of birth of the user
+        
+        public Address Address { get; set; } // Address of the user
     }
+    
 }
